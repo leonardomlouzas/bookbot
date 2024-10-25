cd27e3329a29535600c3340348724a31593075c9
Leonardo Moreira Louzas <leonardomlouzas@gmail.com>
2024-10-25 17:26:15 -0300

    004: add character_count func in main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+def world_count(text):
+    words = text.split()
+    return len(words)
+
+
+def character_count(text):
+    characters = {}
+
+    for character in text:
+        character = character.lower()
+        try:
+            characters[character] += 1
+        except KeyError:
+            characters[character] = 1
+
+    return characters
+
+
 def main():
+    content = ""
     with open("books/frankenstein.txt") as f:
         content = f.read()
-        print(content)
+
+    print(character_count(content))
 
 
 if __name__ == "__main__":
